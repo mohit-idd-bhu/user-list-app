85ad1deb53c2727cd0dfe6618cfd6204c582ecc7
Mohit <mohitsrt123@gmail.com>
2023-03-29 23:21:24 +0530

    first-change protected route not added

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(()=>{
+    const token=localStorage.getItem('isTokenAvailable');
+    if(token){
+      setIsLoggedIn(true);
+    }
+  },[])
+
+  const loginHandler = (email, password) => {
+    if(email==='foo'&&password==='bar'){
+      setIsLoggedIn(true);
+      return;
+    }
+    alert('Invalid UserName or Password');
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+
+const Login = (props) => {
+  const [enteredUserName, setenteredUserName] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  useEffect(()=>{
+    setFormIsValid(
+      enteredPassword.trim().length > 0 && enteredUserName.trim().length > 0
+    )
+  },[enteredUserName,enteredPassword ]);
+
+  const emailChangeHandler = (event) => {
+    setenteredUserName(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(enteredUserName, enteredPassword);
+    setenteredUserName('');
+    setEnteredPassword('');
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="username">User Name</label>
+          <input
+            type="text"
+            id="username"
+            value={enteredUserName}
+            onChange={emailChangeHandler}
+          />
+        </div>
+        <div className={classes.control}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
